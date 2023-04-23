87b5e63c63bfc53723707dbb07652742ce5f9f21
ZakDH <60523522+ZakDH@users.noreply.github.com>
2023-04-23 21:08:00 +0100

    final with comments

--- IoTHub/myIoTHubTrigger.cs
+++ IoTHub/myIoTHubTrigger.cs
@@ -1,5 +1,4 @@
 using IoTHubTrigger = Microsoft.Azure.WebJobs.EventHubTriggerAttribute;
-
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Azure.EventHubs;
@@ -22,6 +21,7 @@ using Microsoft.Azure.Documents.Linq;
 
 namespace Uni.Assignment
 {
+    //class that represents the JSON variable getters and setters
     public class TelemetryData
     {
         [JsonProperty("id")]
@@ -31,9 +31,12 @@ namespace Uni.Assignment
         public int bloodPressureDiastolic {get; set;}
         public double bodyTemperature {get; set;}
     }
+    //class which contains IoT Hub functions
     public class myIoTHubTrigger
     {
         [FunctionName("myIoTHubTrigger")]
+        //function triggers on incoming message from simulation program
+        //sends incoming data to cosmos db
         public static void Run([IoTHubTrigger("messages/events", Connection = "AzureEventHubConnectionString")] EventData message,
         [CosmosDB(databaseName: "IoTData",
                                  collectionName: "TelemetryData",
@@ -41,19 +44,20 @@ namespace Uni.Assignment
                        ILogger log)
         {
             log.LogInformation($"C# IoT Hub trigger function processed a message: {Encoding.UTF8.GetString(message.Body.Array)}");
-        
+            //deserialises the incomign message from JSON to an object
             var jsonBody = Encoding.UTF8.GetString(message.Body);
             dynamic data = JsonConvert.DeserializeObject(jsonBody);
             List<TelemetryData> telemetryDataList = new List<TelemetryData>();
+            //loops through each message item
             foreach (var telemetryJson in data)
             {
-                //extracts values from telemetry JSON
+                //extracts values from telemetry object
                 int heartRate1 = telemetryJson.heartRate;
                 int bloodPressureSystolic1 = telemetryJson.bloodPressureSystolic;
                 int bloodPressureDiastolic1 = telemetryJson.bloodPressureDiastolic;
                 double bodyTemperature1 = telemetryJson.bodyTemperature;
 
-                //creates telemetry data object 
+                //creates telemetry data object and adds them to a list
                 telemetryDataList.Add(new TelemetryData
                 {
                     heartRate = heartRate1,
@@ -62,11 +66,13 @@ namespace Uni.Assignment
                     bodyTemperature = bodyTemperature1
                 });
             }
+            //list is converted to an array and assigned to 'output'
             output = telemetryDataList.ToArray();
         }
-        
+        //function that takes telemetry data from the cosmos db
         [FunctionName("GetTelemetry")]
         public static async Task<IActionResult> GetTelemetryAsync(
+        //uses GET request using the route which holds manditory and optional parameters
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "telemetrydata/{start}/{end}/{filter?}/{sort?}")] HttpRequest req,
         string start, string end, string filter, string sort,
         [CosmosDB(databaseName: "IoTData", 
@@ -74,16 +80,20 @@ namespace Uni.Assignment
         ConnectionStringSetting = "cosmosDBConnectionString")]
         DocumentClient client, ILogger log)
         {
-
+        //parses the timestamp from the route parameter
         DateTimeOffset startTimestamp = DateTimeOffset.Parse(start);
         DateTimeOffset endTimestamp = DateTimeOffset.Parse(end);
-
+        //configures a SQL query which is sent to the cosmos db to get the required data
         var telemetryFields = new string[] { "heartRate", "bloodPressureSystolic", "bloodPressureDiastolic", "bodyTemperature" };
         var selectedFields = filter.Split(',');
+        //finds out what data intersects between the route parameter and the telemetryfields array
         var validFields = selectedFields.Intersect(telemetryFields);
+        //data fields are joined to match cosmos db syntax
         var selectClause = string.Join(",", validFields.Select(f => $"c.{f}"));
         var sortOption = sort?.ToUpper() == "ASC" ? "ASC" : "DESC";
+        //constructed sql query which is used to get required data from cosmos db
         var queryText = $"SELECT c.id, c._ts, {selectClause} FROM c WHERE c._ts >= @startTimestamp AND c._ts <= @endTimestamp ORDER BY c._ts {sortOption}";
+        //converts timestamps from route parameter to unix timeframe
         var query = new SqlQuerySpec
         {
             QueryText = queryText,
@@ -93,22 +103,28 @@ namespace Uni.Assignment
                 new SqlParameter("@endTimestamp", endTimestamp.ToUnixTimeSeconds())
             }
         };
+        //document collection used to parse all the data from cosmos db to more user friendly format
         var collectionLink = UriFactory.CreateDocumentCollectionUri("IoTData", "TelemetryData");
-        
+        //removes data taken from cosmos db. eg: system properties
         var queryOptions = new FeedOptions 
         { 
             EnableCrossPartitionQuery = true,
             PopulateQueryMetrics = false,
         };
+        //adds the data from cosmos db to 'documents' variable
         var documents = client.CreateDocumentQuery<Document>(collectionLink, query, queryOptions).AsDocumentQuery();
 
         var result = await documents.ExecuteNextAsync<Document>();
+        //configures data results from database
         var telemetryData = result.Select(doc => 
         {
+            //adds 'id' and '_ts' fields regardless of the switch method
             var selectedData = new Dictionary<string, object>();
             selectedData.Add("id", doc.GetPropertyValue<string>("id"));
             var timestamp = DateTimeOffset.FromUnixTimeSeconds(doc.GetPropertyValue<long>("_ts"));
+            //converts '._ts' data field into more traditional time format
             selectedData.Add("_ts", timestamp.ToString("yyyy-MM-dd HH:mm:ss"));
+            //used to add data fields if they are selected by user
             foreach (var field in telemetryFields)
             {
                 if (selectedFields.Contains(field))
@@ -132,9 +148,11 @@ namespace Uni.Assignment
             }
             return selectedData;
         });
+        //indexes the received data so it is more user friendly
         var settings = new JsonSerializerSettings{
             Formatting = Formatting.Indented
         };
+        //serialises the object into JSON
         var json = JsonConvert.SerializeObject(telemetryData, settings);
         return new OkObjectResult(json);
         }
